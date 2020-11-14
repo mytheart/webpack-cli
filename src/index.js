6f20f2e1a41b3fb71dc85d112a2ac9457c1457ee
mytheart <550703617@qq.com>
2020-11-14 22:51:24 +0800

    ref:完善webpack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import byteDance from './byteDance.png'
+import printMe from './print.js';
 import './style.css';
+
+console.log('byteDance', byteDance)
+console.log('test')
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = 'hello webpack';
   element.classList.add('hello');
+
+     // 将图像添加到我们现有的 div。
+  var myIcon = new Image();
+  myIcon.src = byteDance;
+
+  element.appendChild(btn);
+  element.appendChild(myIcon);
+
+
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true, // 启动gzip压缩
+    hot: true,
+    port: 9000,
+  },
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].[chunkhash:8].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -12,6 +26,14 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+  ],
 }
