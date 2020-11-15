1352070734ba6c851eb5957659abca039d737f15
mytheart <550703617@qq.com>
2020-11-16 00:24:10 +0800

    ref:配置服务器和接口请求

--- src/print.js
+++ src/print.js
@@ -1,4 +1,11 @@
 export default function printMe() {
-  console.log('I get called from print.js! ');
+  console.log('I get called from print.js! ')
+
+  const url = `/api/student/findAll?appkey=demo13_1545210570249`
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((resp) => {
+      console.log(22, resp)
+    })
 }
 console.log(1234458)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin')
 
 const path = require('path')
 
@@ -8,14 +8,26 @@ module.exports = {
   mode: 'development',
   devtool: 'source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    // contentBase: path.join(__dirname, 'dist'),
+    // index: 'index.html',
+    open: true, // 自动打开浏览器
     compress: true, // 启动gzip压缩
     hot: true,
     port: 9000,
-  },
+    proxy: {
+      '/api': {
+        target: 'http://open.duyiedu.com',
+        changeOrigin: true, // 更改请求头中的host和origin
+      },
+    },
+  },  
+  // stats: {
+  //   modules: false,
+  //   colors: true,
+  // }, // 统计数据，配置控制台输出内容
   entry: {
     app: './src/index.js',
-    print: './src/print.js',
+    // print: './src/print.js',
   },
   output: {
     filename: '[name].[chunkhash:8].js',
@@ -37,9 +49,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ template: './public/index.html' }),
     new CopyPlugin({
-      patterns: [
-        { from: "public/assets", to: 'assets' },
-      ]
-    })
+      patterns: [{ from: 'public/assets', to: 'assets' }],
+    }),
   ],
 }
